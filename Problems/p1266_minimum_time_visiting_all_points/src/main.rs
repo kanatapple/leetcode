d77d031a14c9119958af17dc9cb62c867b4c3426
kanatapple <edamame1224@gmail.com>
2021-02-01 14:48:09 +0900

    Add [1266. Minimum Time Visiting All Points]

---
+++ Problems/p1266_minimum_time_visiting_all_points/src/main.rs
@@ -0,0 +1,53 @@
+struct Solution {}
+
+impl Solution {
+    pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+        let mut seconds = 0;
+        let mut current = vec![points[0][0], points[0][1]];
+
+        let mut index = 0;
+        loop {
+            if points.get(index + 1) == None {
+                break;
+            }
+
+            loop {
+                if current == points[index + 1] {
+                    break;
+                }
+
+                current[0] += if current[0] == points[index + 1][0] {
+                    0
+                } else if current[0] < points[index + 1][0] {
+                    1
+                } else {
+                    -1
+                };
+                current[1] += if current[1] == points[index + 1][1] {
+                    0
+                } else if current[1] < points[index + 1][1] {
+                    1
+                } else {
+                    -1
+                };
+
+                seconds += 1;
+            }
+
+            index += 1;
+        }
+
+        seconds
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Solution::min_time_to_visit_all_points(vec![vec![1, 1], vec![3, 4], vec![-1, 0]]),
+        7
+    );
+    assert_eq!(
+        Solution::min_time_to_visit_all_points(vec![vec![3, 2], vec![-2, 2]]),
+        5
+    );
+}
