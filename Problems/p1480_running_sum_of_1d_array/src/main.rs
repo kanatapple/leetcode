534c18c3461229a037a04d36ba2012d3751bcebe
Yasunari Fujieda <yasunari.fujieda@dena.com>
2021-03-13 17:39:46 +0900

    Add [1480. Running Sum of 1d Array]

---
+++ Problems/p1480_running_sum_of_1d_array/src/main.rs
@@ -0,0 +1,25 @@
+struct Solution;
+
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut sum = 0;
+        let mut sums = Vec::new();
+        nums.iter().for_each(|num| {
+            sum += num;
+            sums.push(sum);
+        });
+        sums
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::running_sum(vec![1, 2, 3, 4]), vec![1, 3, 6, 10]);
+    assert_eq!(
+        Solution::running_sum(vec![1, 1, 1, 1, 1]),
+        vec![1, 2, 3, 4, 5]
+    );
+    assert_eq!(
+        Solution::running_sum(vec![3, 1, 2, 10, 1]),
+        vec![3, 4, 6, 16, 17]
+    );
+}
